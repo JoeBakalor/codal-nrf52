fac78613c08fb09da7b537c2e35111ddad3687e3
jason <jason@robotis.com>
2020-02-21 14:25:07 +0900

    modify constructor to select specific serial device

--- source/NRF52SPI.cpp
+++ source/NRF52SPI.cpp
@@ -30,8 +30,8 @@ DEALINGS IN THE SOFTWARE.
 #include "CodalFiber.h"
 #include "sync_serial.h"
 
-#ifdef NRF_SPIM3
-#define SZLIMIT (p_spim == NRF_SPIM3 ? 0xffff : 0xff)
+#if defined(NRF52840_XXAA) || defined(NRF52833_XXAA)
+#define SZLIMIT 0xffff
 #else
 #define SZLIMIT 0xff
 #endif
@@ -42,10 +42,18 @@ namespace codal
 /**
  * Constructor.
  */
-NRF52SPI::NRF52SPI(Pin &mosi, Pin &miso, Pin &sclk)
+NRF52SPI::NRF52SPI(Pin &mosi, Pin &miso, Pin &sclk, NRF_SPIM_Type* device)
     : codal::SPI(), mosi(mosi), miso(miso), sck(sclk)
 {
-    p_spim = (NRF_SPIM_Type *)allocate_sync_serial(SYNC_SERIAL_MODE_SPIM);
+    if(device == NULL)
+    {
+        p_spim = (NRF_SPIM_Type *)allocate_sync_serial(SYNC_SERIAL_MODE_SPIM);
+    }
+    else
+    {
+        p_spim = (NRF_SPIM_Type *)allocate_sync_serial((void *)device);
+    }
+
     if (!p_spim)
         target_panic(DEVICE_HARDWARE_CONFIGURATION_ERROR);
     IRQn = sync_serial_irqn(p_spim);
